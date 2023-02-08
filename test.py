f8792655b6a8c26b2c6b0354b83650ae5bfc72dc
Chekuri P V S Yogananda Varma <110842297+premsaivarmachekuri@users.noreply.github.com>
2023-02-08 10:27:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import main;
 
 def test1():
-    assert main.capitalize("I love learning") == "I Love Learning"
+    assert main.make_tuples([1,2,3,4], [5,6,7,8]) == [(1,5), (2,6), (3,7), (4,8)]
 def test2():
-    assert main.capitalize("gitam tho sadidham") == "Gitam Tho Sadidham"
+    assert main.make_tuples([1,2.5,3,4], [5,6,7,8]) == [(1,5), (2.5,6), (3,7), (4,8)]
 def test3():
-    assert main.capitalize("do you know CXR") == "Do You Know CXR"
+    assert main.make_tuples(['a','b','c','d'], ['e','f','g','h']) == [('a','e'), ('b','f'), ('c','g'), ('d','h')]
